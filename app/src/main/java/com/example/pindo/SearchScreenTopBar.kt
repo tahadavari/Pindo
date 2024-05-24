3682509f8fcd9f07bd63ae452c5c5f2c57201abd
Taha Davari <tahadavari1381@gmail.com>
2024-05-24 23:53:22 +0330

    search screen

---
+++ app/src/main/java/com/example/pindo/SearchScreenTopBar.kt
@@ -0,0 +1,32 @@
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.pindo.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = { Text(text = "Category") },
+        navigationIcon = {
+            IconButton(onClick = { /* Handle category selection */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_home),
+                    contentDescription = "Category"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* Handle filter action */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_home),
+                    contentDescription = "Filter"
+                )
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/example/pindo/ui/SearchScreen.kt
@@ -0,0 +1,41 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pindo.ui.BottomNavBar
+import com.example.pindo.ui.ProductCard
+
+
+@Composable
+fun SearchScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = { SearchScreenTopBar() },
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SearchBar()
+            LazyColumn(
+                modifier = Modifier
+                    .padding(padding)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(10) {
+                    ProductCard(productName = "test", navController)
+                }
+            }
+        }
+    }
+}

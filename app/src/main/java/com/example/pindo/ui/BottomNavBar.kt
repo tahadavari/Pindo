5441583a6ec29a2768fa785d1d2f24a4fd18339b
Mohammad_Ebrh <mohammad.s13811381@gmail.com>
2024-05-25 00:20:39 +0330

    Adding selected icon

--- app/src/main/java/com/example/pindo/ui/BottomNavBar.kt
+++ app/src/main/java/com/example/pindo/ui/BottomNavBar.kt
@@ -13,11 +13,11 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.pindo.R
 
-sealed class BottomNavItem(var title: String, var icon: Int, var screenRoute: String) {
-    object Home : BottomNavItem("Home", R.drawable.ic_home, "home")
-    object Search : BottomNavItem("Search", R.drawable.ic_search, "search")
-    object Notifications : BottomNavItem("Notifications", R.drawable.ic_notification, "notifications")
-    object Profile : BottomNavItem("Profile", R.drawable.ic_profile, "profile")
+sealed class BottomNavItem(var title: String, var icon: Int, var selectedIcon: Int, var screenRoute: String) {
+    object Home : BottomNavItem("Home", R.drawable.ic_home, R.drawable.ic_home_selected, "home")
+    object Search : BottomNavItem("Search", R.drawable.ic_search, R.drawable.ic_search, "search")
+    object Notifications : BottomNavItem("Notifications", R.drawable.ic_notification, R.drawable.ic_notification, "notifications")
+    object Profile : BottomNavItem("Profile", R.drawable.ic_profile, R.drawable.ic_profile, "profile")
 }
 
 @Composable
@@ -35,7 +35,10 @@ fun BottomNavBar(navController: NavController) {
         val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             NavigationBarItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                icon = {
+                    val icon = if (currentRoute == item.screenRoute) item.selectedIcon else item.icon
+                    Icon(painterResource(id = icon), contentDescription = item.title)
+                },
                 label = { Text(text = item.title, fontSize = 9.sp) },
                 alwaysShowLabel = true,
                 selected = currentRoute == item.screenRoute,

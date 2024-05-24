65500e99c7bb683d8bbc071de5666b318980eb36
Mohammad_Ebrh <mohammad.s13811381@gmail.com>
2024-05-24 23:43:06 +0330

    Adding Lazy column

--- app/src/main/java/com/example/pindo/ui/TopOfferSection.kt
+++ app/src/main/java/com/example/pindo/ui/TopOfferSection.kt
@@ -1,6 +1,8 @@
 package com.example.pindo.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,16 +33,29 @@ fun TopOfferSection() {
         )
     }
     Spacer(modifier = Modifier.height(24.dp))
-    Column {
-        TopOfferItem(rank = 1, productName = "Product 02", price = "6$", originalPrice = "20$", discount = "+71.9%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
-        TopOfferItem(rank = 2, productName = "Product 03", price = "24$", originalPrice = "60$", discount = "+64.1%")
+    LazyColumn {
+        items(
+            listOf(
+                Pair(1, "Product 02"),
+                Pair(2, "Product 03"),
+                Pair(3, "Product 04"),
+                Pair(4, "Product 05"),
+                Pair(5, "Product 06"),
+                Pair(6, "Product 07"),
+                Pair(7, "Product 08"),
+                Pair(8, "Product 09"),
+                Pair(9, "Product 10"),
+                Pair(10, "Product 11"),
+                Pair(11, "Product 12")
+            )
+        ) { item ->
+            TopOfferItem(
+                rank = item.first,
+                productName = item.second,
+                price = "24$",
+                originalPrice = "60$",
+                discount = "+64.1%"
+            )
+        }
     }
 }
